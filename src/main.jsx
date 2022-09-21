6227aa7c2df2d32793f7a56edc7a234055f10088
Guilherme Fernandes <guilheermeff@outlook.com>
2022-09-21 18:52:58 -0400

    add packages

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Details } from './pages/Details';
+
+import { ThemeProvider } from 'styled-components';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Details />
   </React.StrictMode>
 )
