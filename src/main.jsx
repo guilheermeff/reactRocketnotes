13e4ffdc8f0b58dafc391d50a2a47f2d5890bedb
Guilherme Fernandes <guilheermeff@outlook.com>
2022-09-21 19:01:16 -0400

    add ThemeProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Details } from './pages/Details';
+import theme from './styles/theme';  
 
 import { ThemeProvider } from 'styled-components';
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Details />
+  <React.StrictMode theme={theme}>
+    <ThemeProvider>
+      <Details />
+    </ThemeProvider>
   </React.StrictMode>
 )
