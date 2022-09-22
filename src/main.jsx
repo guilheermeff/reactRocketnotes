c161e8c42e22600861ef6de497b8df9ef0f62543
Guilherme Fernandes <guilheermeff@outlook.com>
2022-09-21 20:52:21 -0400

    add GlobalStyles

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Details } from './pages/Details';
+import  GlobalStyles from './styles/global.js';
 import theme from './styles/theme.js';  
 
 import { ThemeProvider } from 'styled-components';
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Details />
+      <GlobalStyles />
     </ThemeProvider>
   </React.StrictMode>
 )
