ef451398f337404549444175de2f364afff8eeb4
Guilherme Fernandes <guilheermeff@outlook.com>
2022-09-23 16:59:14 -0400

    create component section

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,5 +1,6 @@
 import { Container } from './styles.js';
 import { Header } from '../../components/Header/index.jsx';
+import { Section } from '../../components/Section/index.jsx';
 import { Button } from '../../components/Button/index.jsx';
 
 export function Details() {
@@ -7,6 +8,15 @@ export function Details() {
   return (
     <Container>
       <Header/>
+
+      <Section title="links úteis">
+        <ul>
+          <li>Ítem 1</li>
+          <li>Ítem 2</li>
+          <li>Ítem 3</li>
+        </ul>
+      </Section>
+
       <Button title="Voltar"/>
     </Container>
   )
