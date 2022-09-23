f92c6a1a56780f9bcd73f4355d88a82da282d23f
Guilherme Fernandes <guilheermeff@outlook.com>
2022-09-23 16:00:51 -0400

    create shutDown icon

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,5 @@
-import { Container, Profile } from "./styles";
+import { RiShutDownLine } from 'react-icons/ri';
+import { Container, Profile, Logout } from "./styles";
 
 
 export function Header() {
@@ -12,6 +13,10 @@ export function Header() {
           <strong>Guilherme Fernandes</strong>
         </div>
       </Profile>
+
+      <Logout>
+        <RiShutDownLine/>
+      </Logout>
     </Container>
   )
 }
