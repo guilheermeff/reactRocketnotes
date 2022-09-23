189cd82dc57b01be2e8c76a6bae0af81481174f0
Guilherme Fernandes <guilheermeff@outlook.com>
2022-09-23 09:53:41 -0400

    create component Header

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,17 +1,13 @@
 import { Container } from './styles.js';
+import { Header } from '../../components/Header/index.jsx';
 import { Button } from '../../components/Button/index.jsx';
 
 export function Details() {
 
   return (
     <Container>
-      <h1>Hello World!</h1>
-      <span>Guilherme Feitosa Fernandes</span>
-
-      <Button title="Abrir" loading/>
-      <Button title="Cadastrar" loading/>
-      <Button title="Cancelar" loading/>
-      <Button title="Voltar" loading/>
+      <Header/>
+      <Button title="Voltar"/>
     </Container>
   )
 }
