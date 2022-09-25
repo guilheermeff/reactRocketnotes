dfa6fb35c299fb5d5cc92eee5038939c1f665ec8
Guilherme Fernandes <guilheermeff@outlook.com>
2022-09-25 16:28:59 -0400

    create Note component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,6 +3,7 @@ import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
 import { Header } from '../../components/Header';
 import { ButtonText } from '../../components/ButtonText';
 import { Input } from '../../components/Input';
+import { Section } from '../../components/Section';
 import { FiPlus } from 'react-icons/fi';
 
 export function Home() {
@@ -26,7 +27,9 @@ export function Home() {
       </Search>
 
       <Content>
+        <Section title="Minhas notas" />
 
+        
       </Content>
 
       <NewNote>
