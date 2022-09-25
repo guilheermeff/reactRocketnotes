2da3750977695f11fe4937c206e683abedc913f1
Guilherme Fernandes <guilheermeff@outlook.com>
2022-09-25 17:47:24 -0400

    fix bug

--- src/components/Note/index.jsx
+++ src/components/Note/index.jsx
@@ -10,9 +10,9 @@ export function Note({ data, ...rest }) {
         data.tags &&
         <footer>
           {
-            data.tags.map(tag => {
+            data.tags.map(tag => 
               <Tag key={tag.id} title={tag.name} />
-            })
+            )
           }
         </footer>
       }
