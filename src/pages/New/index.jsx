d0512a435917eae23131ff2ba551760a9e20f489
Guilherme Fernandes <guilheermeff@outlook.com>
2022-09-28 18:28:24 -0400

    updates

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -3,6 +3,7 @@ import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
 import { TextArea } from '../../components/TextArea';
 import { Section } from '../../components/Section';
+import { NoteItem } from '../../components/NoteItem';
 import { Button } from '../../components/Button';
 
 export function New() {
@@ -22,7 +23,7 @@ export function New() {
           <TextArea placeholder="Observações"/>
 
           <Section title="Links úteis">
-            
+            <NoteItem value="https://www.globo.com.br/" />
           </Section>
 
           <Button title="Salvar" />
