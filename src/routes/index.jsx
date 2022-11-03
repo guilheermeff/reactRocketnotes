1e455125d7555e0e45abe880ebc30d96e2cd8c38
Guilherme Fernandes <guilheermeff@outlook.com>
2022-11-03 14:49:28 -0400

    authentication data to display the routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter } from "react-router-dom";
+import { useAuth } from "../hooks/auth.jsx";
 
 import { AuthRoutes } from "./auth.routes.jsx";
 import { AppRoutes } from './app.routes.jsx';
 
 
 export function Routes() {
+
+  const { user } = useAuth();
+
   return(
     <BrowserRouter>
-      <AuthRoutes />
-      <AppRoutes />
+      {user ? <AppRoutes /> : <AuthRoutes />}
     </BrowserRouter>
   )
 }
